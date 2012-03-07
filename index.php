372e729febeae20a1d42dca7ffb64137e566de6c
Mattias Runge <mattias@runge.se>
2012-03-07 11:21:39 +0100

    Import of HTML 5 boilerplate with some modifications. Import of latest jQuery and jQuery UI.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8"/>
+
+    <title></title>
+
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <meta name="viewport" content="width=device-width"/>
+
+    <link type="text/css" rel="stylesheet" href="css/ui-lightness/jquery-ui-1.8.18.custom.css"/>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+    <script src="js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+    <header>
+    </header>
+    
+    <div role="main">
+    </div>
+    
+    <footer>
+    </footer>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-ui-1.8.18.custom.min.js"><\/script>')</script>
+</body>
+</html>
