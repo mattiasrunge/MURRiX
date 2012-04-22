61cb97fb67ff58df366aa7a942615452b2fa4cd3
Mattias Runge <mattias@runge.se>
2012-04-23 00:09:00 +0200

    Added tipsy library for tooltips Added datetime library Moment Added initial module for timeline Added support for position table in the DB API A basic panel layout added which now loads google maps

---
+++ murrix/modules/timeline/murrix.timeline.php
@@ -0,0 +1,23 @@
+<?
+
+$files = array
+(
+  "php" => array
+  (
+    "murrix.timeline.backend.php"
+  ),
+  "js" => array
+  (
+    "murrix.timeline.js"
+  ),
+  "tmpl" => array
+  (
+    "murrix-timeline-container"     => "templates/murrix.timeline.container.html",
+  ),
+  "css" => array
+  (
+    "murrix.timeline.css",
+  ),
+);
+
+?>
