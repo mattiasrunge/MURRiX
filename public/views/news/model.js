3df7f36d82e9617df672084218c74586b8cd0faa
Mattias Runge <mattias@runge.se>
2012-10-30 22:14:56 +0100

    Added sorting option on node find backend API. Implemented a rudimentry news page, probably needs a layout overhaul... and should show new items and comments etc also

--- public/views/news/model.js
+++ public/views/news/model.js
@@ -7,12 +7,39 @@ var NewsModel = function(parentModel)
   parentModel.path().secondary.subscribe(function(value) { murrix.updatePath(value, self.path); });
 
   self.show = ko.observable(false);
+  self.nodes = ko.observableArray();
 
   parentModel.path().primary.subscribe(function(value)
   {
     if (self.show() !== (value.action === "news"))
     {
       self.show(value.action === "news");
+    }
+  });
+
+  self.show.subscribe(function(value)
+  {
+    self.nodes.removeAll();
+  
+    if (value)
+    {
+      murrix.server.emit("find", { query: { }, options: { collection: "nodes", limit: 25, sort: "modified.timestamp", sortDirection: "desc" } }, function(error, nodeDataList)
+      {
+        if (error)
+        {
+          console.log(error);
+          callback([]);
+        }
+
+        var nodeList = [];
+
+        for (var key in nodeDataList)
+        {
+          nodeList.push(murrix.cache.addNodeData(nodeDataList[key]));
+        }
+
+        self.nodes(nodeList);
+      });
     }
   });
 };
