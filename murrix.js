3268610cf4d199891a36aff03df6aec7ef6dbe0c
Mattias Runge <mattias@runge.se>
2014-01-04 21:10:46 +0100

    A bit of work on the backend startup. Now reads a configuration file that have got a new format. Added database dump functionallity.

--- murrix.js
+++ murrix.js
@@ -1,85 +1,66 @@
 
-/* Includes, TODO: Sanitize case */
 var path = require("path");
-var events = require("events");
-var util = require("util");
-var ewait = require("ewait");
-
 var Server = require("./lib/server");
-var db = require("./lib/db");
 var sessions = require("./lib/session");
+var db = require("./lib/db");
+var media = require("./lib/media");
+var dbdump = require("./lib/dbdump");
 
-// var MurrixLoggerManager = require("./lib/logger.js").Manager;
-// var MurrixConfigurationManager = require("./lib/config.js").Manager;
-// var MurrixDatabaseManager = require("./lib/db.js").Manager;
-// var MurrixCacheManager = require("./lib/cache.js").Manager;
-// var MurrixUserManager = require("./lib/user.js").Manager;
-// var MurrixGroupManager = require("./lib/group.js").Manager;
-// var MurrixTriggersManager = require("./lib/triggers.js").Manager;
-// var MurrixHelpersManager = require("./lib/helpers.js").Manager;
-// var MurrixImportManager = require("./lib/import.js").Manager;
-// var MurrixClientManager = require("./lib/client.js").Manager;
-// var MurrixServerManager = require("./lib/server.js").Manager;
-// var MurrixBackupManager = require("./lib/backup.js").Manager;
-// //var MurrixScrapersManager = require("./lib/scrapers.js").Manager;
-// var MurrixSessionManager = require("./lib/session.js").Manager;
+var Logger = require("./lib/logger");
+var Chain = require("achain.js");
 
-function Murrix(options)
+var logger = new Logger("main");
+
+exports.Murrix = function(options)
 {
-//   events.EventEmitter.call(this);
+  var chain = new Chain();
+  var server = new Server();
+  
+  logger.info("Loading configuration from " + options.config + "...");
+  var config = require(options.config);
+  config = config || {};
 
-  var self = this;
-
-  self.name = "murrix";
-  self.basePath = function() { return __dirname + "/"; };
-  self.server = new Server();
-
-  sessions.loadFromDisk(self.basePath() + "sessions/");
-
-  db.open("localhost", 27017, "murrix_devel", function(error)
+  chain.add(function(args, options, next)
   {
-    self.server.start();
+    sessions.initialize(config, next);
   });
+  
+  chain.add(function(args, options, next)
+  {
+    config.db = config .db || {};
+    
+    var hostname = config.db.hostname || "localhost";
+    var port = config.db.port || 27017;
+    var name = config.db.name || "murrix";
+    
+    db.open(hostname, port, name, next);
+  });
+  
+  chain.add(function(args, options, next)
+  {
+    dbdump.initialize(config, next);
+  });
+  
+  chain.add(function(args, options, next)
+  {
+    media.initialize(config, next);
+  });
+  
+  chain.add(function(args, options, next)
+  {
+    server.initialize(config, next);
+  });
+  
+  chain.run(function(error)
+  {
+    if (error)
+    {
+      logger.error("Faild on startup, error: " + error);
+      process.exit(255);
+    }
+  });
+};
 
-
-
-//   self.emit("done");
-
-
-//   self.on("init", function()
-//   {
-//     self.logger.info(self.name, "Initializing MURRiX...");
-//   });
-//
-//   self.on("done", function()
-//   {
-//     self.logger.info(self.name, "Initialization complete!");
-//   });
-//
-//   var modules = [];
-//
-//   self.utils = require("./lib/utils");
-//
-//   self.config = new MurrixConfigurationManager(self, path.resolve(self.basePath(), options.config));
-//   modules.push(self.config);
-//
-//   self.logger = new MurrixLoggerManager(self);
-//   modules.push(self.logger);
-//
-//   self.session = new MurrixSessionManager(self);
-//   modules.push(self.session);
-//
-//   self.client = new MurrixClientManager(self);
-//   modules.push(self.client);
-//
-//   self.db = new MurrixDatabaseManager(self);
-//   modules.push(self.db);
-//
-//   self.user = new MurrixUserManager(self);
-//   modules.push(self.user);
-//
-//   self.group = new MurrixGroupManager(self);
-//   modules.push(self.group);
 //
 //   self.cache = new MurrixCacheManager(self);
 //   modules.push(self.cache);
@@ -87,33 +68,12 @@ function Murrix(options)
 //   self.import = new MurrixImportManager(self);
 //   modules.push(self.import);
 //
-//   self.helpers = new MurrixHelpersManager(self);
-//   modules.push(self.helpers);
-//
 //   self.triggers = new MurrixTriggersManager(self, path.resolve(self.basePath(), "./triggers.json"));
 //   modules.push(self.triggers);
-//
-//   self.server = new MurrixServerManager(self);
-//   modules.push(self.server);
 //
 //   self.backup = new MurrixBackupManager(self);
 //   modules.push(self.backup);
 //
-// //   self.scrapers = new MurrixScrapersManager(self);
-// //   modules.push(self.scrapers);
+//   self.scrapers = new MurrixScrapersManager(self);
+//   modules.push(self.scrapers);
 //
-//   var wait = new ewait.WaitForAll();
-//
-//   wait.add(modules);
-//
-//   wait.on("done", function()
-//   {
-//     self.emit("done");
-//   });
-
-//   wait.wait();
-}
-
-// util.inherits(Murrix, events.EventEmitter);
-
-exports.Murrix = Murrix;
