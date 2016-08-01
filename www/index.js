e85175fbdf955e9829a8bee9c5a9ba8deec1f26c
Mattias Runge <mattias@runge.se>
2016-08-01 14:09:05 +0200

    Updated sample configuration file
    Added a nice printout if not using chrome

--- www/index.js
+++ www/index.js
@@ -1,50 +1,56 @@
 "use strict";
 
-require.config({
-    baseUrl: ".",
-    paths: {
-        text: "node_modules/requirejs-text/text",
-        json: "node_modules/requirejs-json/json",
-        jquery: "node_modules/jquery/dist/jquery.min",
-        knockout: "node_modules/knockout/build/output/knockout-latest",
-        bootstrap: "node_modules/bootstrap/dist/js/bootstrap.min",
-        typeahead: "node_modules/corejs-typeahead/dist/typeahead.jquery",
-        slider: "node_modules/bootstrap-slider/dist/bootstrap-slider.min",
-        mprogress: "node_modules/mprogress/build/js/mprogress.min",
-        moment: "node_modules/moment/min/moment.min",
-        snackbar: "node_modules/snackbarjs/dist/snackbar.min",
-        autosize: "node_modules/autosize/dist/autosize.min",
-        lazyload: "node_modules/vanilla-lazyload/dist/lazyload",
-        "socket.io-client": "/socket.io/socket.io",
-        "api.io-client": "/api.io/api.io-client",
-        "co": "/api.io/co"
-    },
-    shim: {
-        bootstrap: {
-            deps: [ "jquery" ]
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+if (!isChrome) {
+    document.getElementById("browserWarning").className = "";
+} else {
+    require.config({
+        baseUrl: ".",
+        paths: {
+            text: "node_modules/requirejs-text/text",
+            json: "node_modules/requirejs-json/json",
+            jquery: "node_modules/jquery/dist/jquery.min",
+            knockout: "node_modules/knockout/build/output/knockout-latest",
+            bootstrap: "node_modules/bootstrap/dist/js/bootstrap.min",
+            typeahead: "node_modules/corejs-typeahead/dist/typeahead.jquery",
+            slider: "node_modules/bootstrap-slider/dist/bootstrap-slider.min",
+            mprogress: "node_modules/mprogress/build/js/mprogress.min",
+            moment: "node_modules/moment/min/moment.min",
+            snackbar: "node_modules/snackbarjs/dist/snackbar.min",
+            autosize: "node_modules/autosize/dist/autosize.min",
+            lazyload: "node_modules/vanilla-lazyload/dist/lazyload",
+            "socket.io-client": "/socket.io/socket.io",
+            "api.io-client": "/api.io/api.io-client",
+            "co": "/api.io/co"
         },
-        typeahead: {
-            deps: [ "jquery" ]
-        },
-        snackbar: {
-            deps: [ "jquery" ]
-        },
-        slider: {
-            deps: [ "jquery" ]
+        shim: {
+            bootstrap: {
+                deps: [ "jquery" ]
+            },
+            typeahead: {
+                deps: [ "jquery" ]
+            },
+            snackbar: {
+                deps: [ "jquery" ]
+            },
+            slider: {
+                deps: [ "jquery" ]
+            }
         }
-    }
-});
-
-define([ "lib/main" ], (main) => {
-    let args = {
-        hostname: location.hostname,
-        port: location.port
-    };
-
-    main.start(args)
-    .catch((error) => {
-        console.error("FATAL ERROR");
-        console.error(error);
-        console.error(error.stack);
     });
-});
+
+    define([ "lib/main" ], (main) => {
+        let args = {
+            hostname: location.hostname,
+            port: location.port
+        };
+
+        main.start(args)
+        .catch((error) => {
+            console.error("FATAL ERROR");
+            console.error(error);
+            console.error(error.stack);
+        });
+    });
+}
