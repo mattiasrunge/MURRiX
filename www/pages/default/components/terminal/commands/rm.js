f6fcde5ad4fd05e565f66ba39ebbd98b93730f91
Mattias Runge <mattias@runge.se>
2017-11-04 14:53:58 +0100

    Added wildcard support to rm command

--- www/pages/default/components/terminal/commands/rm.js
+++ www/pages/default/components/terminal/commands/rm.js
@@ -9,18 +9,23 @@ export default {
     },
     exec: async (term, streams, cmd, opts, args) => {
         const abspath = await term.getAbspath(args.path, false);
+        const slashIndex = abspath.lastIndexOf("/");
+        const lastPart = abspath.substr(slashIndex + 1);
+        const items = lastPart.includes("*") ? await api.vfs.list(abspath) : [ await api.vfs.resolve(abspath, { nodepath: true }) ];
 
-        if (opts.f) {
-            await api.vfs.unlink(abspath);
+        for (const item of items) {
+            if (opts.f) {
+                await api.vfs.unlink(item.path);
 
-            return;
-        }
+                return;
+            }
 
-        term.setPrompt({ prompt: `Are you sure you want to remove <span class='bold'>${abspath}</span>? [y/N]` });
-        const answer = await streams.stdin.read();
+            term.setPrompt({ prompt: `Are you sure you want to remove <span class='bold'>${item.name}</span>? [y/N]` });
+            const answer = await streams.stdin.read();
 
-        if (answer.toLowerCase() === "y") {
-            await api.vfs.unlink(abspath);
+            if (answer.toLowerCase() === "y") {
+                await api.vfs.unlink(item.path);
+            }
         }
     },
     completion: async (term, cmd, name, value) => {
