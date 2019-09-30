112db69d7993d5996922b22e7eb9d4716d0bec46
Mattias Runge-Broberg <mattias@runge.se>
2019-09-30 20:29:21 +0200

    Allow construction of cache filename without asking mcs

--- core/lib/packages/media/commands/url.js
+++ core/lib/packages/media/commands/url.js
@@ -2,11 +2,46 @@
 
 const assert = require("assert");
 const path = require("path");
+const fs = require("fs-extra");
 const Node = require("../../../core/Node");
 const { ADMIN_CLIENT } = require("../../../core/auth");
 const config = require("../../../configuration");
 const media = require("../../../media");
+const log = require("../../../log")(module);
 const syncmedia = require("./syncmedia");
+
+const getExtensionByType = (type) => {
+    if (type === "image") {
+        return "jpg";
+    } else if (type === "video" || type === "audio") {
+        return "webm";
+    } else if (type === "document") {
+        return "pdf";
+    } else if (type === "*") {
+        return;
+    }
+
+    throw new Error(`Unknown format type, ${type}`);
+};
+
+const constructFilename = (id, format) => {
+    const square = format.width === format.height ? 1 : 0;
+    const extension = getExtensionByType(format.type);
+    const height = format.height || 0;
+    const width = format.width || 0;
+    const angle = format.angle || 0;
+    const mirror = format.mirror ? 1 : 0;
+
+    return [
+        id,
+        "_w", width,
+        "_h", height,
+        "_s", square,
+        "_a", angle,
+        "_m", mirror,
+        ".", extension
+    ].join("");
+};
 
 module.exports = async (client, abspath, format) => {
     try {
@@ -28,28 +63,46 @@ module.exports = async (client, abspath, format) => {
             mirror: format.mirror || node.attributes.mirror,
             width: format.width,
             height: format.height,
-            type: format.type || node.attributes.type
+            type: format.type || node.attributes.type,
+            timeindex: node.attributes.timeindex
         };
 
-        // TODO: Check if format exists in node.attributes.cached, then no need to contact mcs
+        if (cacheformat.angle) {
+            cacheformat.angle = parseInt(cacheformat.angle, 10);
 
-        // Get cached name and create if not existing
-        const cached = await media.getCached(node._id, filename, {
-            timeindex: node.attributes.timeindex,
-            ...cacheformat
-        });
-
-        if (!cached) {
-            return null;
+            if (cacheformat.angle === 0 || isNaN(cacheformat.angle)) {
+                delete cacheformat.angle;
+            } else if (![ 90, 180, 270 ].includes(Math.abs(cacheformat.angle))) {
+                throw new Error("Valid angle values are: -270, -180, -90, 90, 180 and 270");
+            }
         }
 
-        if (node.attributes.cached) {
-            await syncmedia(client, node);
+        if (cacheformat.timeindex === 0 || isNaN(cacheformat.timeindex)) {
+            delete cacheformat.timeindex;
+        }
+
+        let filepath = path.join(config.mcsDirectory, constructFilename(node._id, cacheformat));
+
+        try {
+            await fs.access(filepath);
+        } catch {
+            // Get cached name and create if not existing
+            filepath = await media.getCached(node._id, filename, cacheformat);
+
+            if (!filepath) {
+                return null;
+            }
+
+            if (node.attributes.cached) {
+                await syncmedia(client, node);
+            }
         }
 
         // Return url
-        return media.createCacheUrl(path.basename(cached), node.attributes.name);
+        return media.createCacheUrl(path.basename(filepath), node.attributes.name);
     } catch (error) {
+        log.error(error, abspath, format);
+
         return null;
     }
 };
