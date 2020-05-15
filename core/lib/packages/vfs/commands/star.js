90debc0b1a7ddffea18d3dba60bc7d5b2a707ccd
Mattias Runge-Broberg <mattias@runge.se>
2020-05-15 11:20:06 +0200

    Added ensure stars directory to stars commands

--- core/lib/packages/vfs/commands/star.js
+++ core/lib/packages/vfs/commands/star.js
@@ -5,11 +5,14 @@ const Node = require("../../../core/Node");
 const { ADMIN_CLIENT } = require("../../../core/auth");
 const unlink = require("./unlink");
 const symlink = require("./symlink");
+const ensure = require("./ensure");
 
 module.exports = async (client, abspath) => {
     assert(!client.isGuest(), "Permission denied");
 
     const starspath = `/users/${client.getUsername()}/stars`;
+    await ensure(ADMIN_CLIENT, starspath, "d");
+
     const list = await Node.list(ADMIN_CLIENT, starspath, { nofollow: true });
     const star = list.find((node) => node.attributes.path === abspath);
 
--- core/lib/packages/vfs/commands/stars.js
+++ core/lib/packages/vfs/commands/stars.js
@@ -3,11 +3,14 @@
 const assert = require("assert");
 const Node = require("../../../core/Node");
 const { ADMIN_CLIENT } = require("../../../core/auth");
+const ensure = require("./ensure");
 
 module.exports = async (client) => {
     assert(!client.isGuest(), "Permission denied");
 
     const abspath = `/users/${client.getUsername()}/stars`;
+    await ensure(ADMIN_CLIENT, abspath, "d");
+
     const list = await Node.list(ADMIN_CLIENT, abspath);
 
     return Promise.all(list.map((nodepath) => nodepath.serialize(client)));
