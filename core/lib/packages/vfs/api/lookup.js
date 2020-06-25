63a3ae0dfa61954fd1e734418b1a0831168a72cd
Mattias Runge-Broberg <mattias@runge.se>
2020-06-25 20:34:40 +0200

    Added possibillity to fix incorrect ref count and added command for printing node id

--- core/lib/packages/vfs/api/lookup.js
+++ core/lib/packages/vfs/api/lookup.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const assert = require("assert");
 const Node = require("../../../lib/Node");
+const log = require("../../../lib/log")(module);
 
-module.exports = async (client, id) => await Node.lookup(client, id);
+module.exports = async (client, id, options) => {
+    const nodes = await Node.lookup(client, id);
+
+    if (options.fix) {
+        assert(client.isAdmin(), "Only admin can fix ref counts");
+
+        if (nodes.length > 0 && nodes[0].properties.count !== nodes.length) {
+            log.info(`Count for node ${nodes[0]._id} did not match the returned number of paths ${nodes.length}, will fix...`);
+
+            await nodes[0]._props(client, { count: nodes.length });
+
+            nodes.forEach((node) => node.properties.count = nodes.length);
+        }
+    }
+
+    await Promise.all(nodes.map((node) => node.serialize(client)));
+};
--- core/lib/packages/vfs/commands/lookup.js
+++ core/lib/packages/vfs/commands/lookup.js
@@ -4,10 +4,12 @@ const { api } = require("../../../api");
 
 module.exports = async (client, term,
     // Translate node id to path
-    opts,
+    opts, // f Try to fix incorrect reference count
     id // Generic
 ) => {
-    const nodes = await api.lookup(client, id);
+    const nodes = await api.lookup(client, id, {
+        fix: !!opts.f
+    });
 
     for (const node of nodes) {
         term.writeln(node.path);
