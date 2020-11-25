2aab2da29acffb2f59e99b313a923b481fe92330
Mattias Runge-Broberg <mattias@runge.se>
2020-11-25 11:31:30 +0100

    Fix

--- core/lib/packages/labels/api/randombylabel.js
+++ core/lib/packages/labels/api/randombylabel.js
@@ -11,13 +11,10 @@ module.exports = async (client, label, types, excludePaths = []) => {
     .filter((node) => !excludePaths.includes(node.path))
     .filter((node) => types.includes(node.properties.type));
 
-    while (list.length > 0) {
+    if (list.length > 0) {
         const index = Math.floor(Math.random() * list.length);
-        const node = list.splice(index, 1)[0];
 
-        if (await node.hasAccess(client, "r")) {
-            return node.serialize(client);
-        }
+        return list.splice(index, 1)[0];
     }
 
     throw new Error("Could not find a readable random node based on label");
